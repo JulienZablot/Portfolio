2f01c14e59f24e6e665c0c0f9847d19e346f21d9
JulienZablot <109862451+JulienZablot@users.noreply.github.com>
2022-07-25 09:53:29 +0200

    Add files via upload

---
+++ assets/vendor/form-without-php/form.js
@@ -0,0 +1,31 @@
+
+  var form = document.getElementById("my-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = '<div class="text-center">Votre message a bien été envoyé</div>';
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = '<div class="text-center">"Un problème est survenu, veuillez de nouveau envoyer votre message</div>'
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = '<div class="text-center">"Un problème est survenu, veuillez de nouveau envoyer votre message</div>'
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
